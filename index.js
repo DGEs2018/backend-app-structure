213124f0e65ad3c4381c1820b766926572074dd3
Dawit <dawit@ginetta.net>
2020-06-09 11:56:49 +0200

    Put in an array of objects stored in a variable called lessons

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 console.log('Hello World!');
 
-const http = require('http');
+const http = require('http'); // imports Node's built-in web-server module
 
+let lessons = [
+	{
+		id: 1,
+		content: 'What first content is about',
+		date: '2020-06-09T17:30:31.098Z',
+		important: false
+	},
+	{
+		id: 2,
+		content: 'Whatever second content is about',
+		date: '2020-06-09T17:30:31.098Z',
+		important: false
+	},
+	{
+		id: 3,
+		content: 'Whatever third content is about',
+		date: '2020-06-09T17:30:31.098Z',
+		important: true
+	}
+];
 const app = http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.end('Hello World');
 });
 
 const port = 3001;
-
 app.listen(port);
-
 console.log(`Server running on port ${port}`);
