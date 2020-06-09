807dd931fd912da2793c09962159e272e4fe5139
Dawit <dawit@ginetta.net>
2020-06-09 16:11:12 +0200

    Install express as well as nodemon, define two routes to the application

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-console.log('Hello World!');
+// console.log('Hello World!');
 
-const http = require('http'); // imports Node's built-in web-server module
+// const http = require('http'); // imports Node's built-in web-server module
+const express = require('express');
+
+const app = express();
 
 let lessons = [
 	{
@@ -22,10 +25,18 @@ let lessons = [
 		important: true
 	}
 ];
-const app = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello World');
+app.get('/', (req, res) => {
+	res.send('<h1>Trying Express</h1>');
 });
+
+app.get('/api/lessons', (req, res) => {
+	res.json(lessons);
+});
+
+// const app = http.createServer((req, res) => {
+// 	res.writeHead(200, { 'Content-Type': 'application/json' });
+// 	res.end(JSON.stringify(lessons));
+// });
 
 const port = 3001;
 app.listen(port);
